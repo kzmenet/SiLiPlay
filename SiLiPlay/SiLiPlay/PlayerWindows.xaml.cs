1e9297a512cea2926d2492df596fa66792de2f1d
M-IoT <kzmenet@gmail.com>
2016-07-21 23:12:11 +0900

    MainWindowのGUI整備、メインウインドウ終了時に小ウィンドウのclose

--- SiLiPlay/SiLiPlay/PlayerWindows.xaml.cs
+++ SiLiPlay/SiLiPlay/PlayerWindows.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SiLiPlay
 {
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    /// PlayerWindows.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PlayerWindows : Window
     {
-        PlayerWindows playwindow = new PlayerWindows();
-        public MainWindow()
+        public PlayerWindows()
         {
             InitializeComponent();
-            playwindow.Show();
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            playwindow.Close();
         }
     }
 }
